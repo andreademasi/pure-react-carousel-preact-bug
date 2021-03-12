8d43d79f4722e7a4427e923a51b542c277e147cb
Andrea De Masi <andreademasi80@gmail.com>
2021-03-12 20:40:10 +0100

    Bug reproduction

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,63 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+} from "pure-react-carousel"
+import "pure-react-carousel/dist/react-carousel.es.css"
 
 import Layout from "../components/layout"
-import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+    <CarouselProvider
+      totalSlides={4}
+      currentSlide={2}
+      naturalSlideWidth={1}
+      naturalSlideHeight={1.25}
+    >
+      <Slider className="slider">
+        <Slide
+          index={0}
+          classNameHidden="hiddenSlide"
+          classNameVisible="visibleSlide"
+          className="slide"
+        >
+          FIRST SLIDE
+        </Slide>
+        <Slide
+          index={1}
+          classNameHidden="hiddenSlide"
+          classNameVisible="visibleSlide"
+          className="slide"
+        >
+          SECOND SLIDE
+        </Slide>
+        <Slide
+          index={2}
+          classNameHidden="hiddenSlide"
+          classNameVisible="visibleSlide"
+          className="slide"
+        >
+          THIRD SLIDE
+        </Slide>
+        <Slide
+          index={3}
+          classNameHidden="hiddenSlide"
+          classNameVisible="visibleSlide"
+          className="slide"
+        >
+          FOURTH SLIDE
+        </Slide>
+      </Slider>
+      <ButtonBack >
+      </ButtonBack>
+      <ButtonNext >
+      </ButtonNext>
+    </CarouselProvider>
+
   </Layout>
 )
 
